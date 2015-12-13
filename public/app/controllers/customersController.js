214993965d11f81264abf5802f4d4a214e1b0bda
comountainclimber <mlasky46@gmail.com>
2015-12-13 12:30:17 -0700

    ...

--- public/app/controllers/customersController.js
+++ public/app/controllers/customersController.js
@@ -1,49 +1,16 @@
 (function(){
 	angular.module('customersApp')
-	.controller('customerController', ['$scope', function($scope){
+	.controller('customerController', ['$scope', 'customersFactory', function($scope, customersFactory){
 
 		$scope.sortBy = 'name'
 		$scope.reverse = false
+		$scope.customers = []
 
-		$scope.customers = [{id: 1,
-							joined: '2008-03-10', 
-							name: 'Steve Freeman', 
-							city: 'Cranford', 
-							orderTotal: 666.6666,
-							orders: [
-								{
-									id:1,
-									product: 'India Pale Ale',
-									total: 666.6666 
-								}
-							]
-							},
+		function init(){
+			$scope.customers = customersFactory.getCustomers()
+		}
 
-							{id:2,
-							joined: '2000-12-02', 
-							name: 'Mick Jagger', 
-							city: 'NYC', 
-							orderTotal: 56.32,
-							orders: [
-								{
-									id:2,
-									product: 'TV',
-									total: 50.00 
-								},
-								{
-									id:3,
-									product: 'Diamonds',
-									total: 6.32 
-								}
-
-							]	
-							},
-
-							{id:3,
-							joined: '2000-12-02',
-							name: 'Pat Doucher', 
-							city: 'Westfield', 
-							orderTotal: 420}]
+		init();
 
 		$scope.doSort = function(propName){
 			$scope.sortBy=propName
--- public/app/controllers/ordersController.js
+++ public/app/controllers/ordersController.js
@@ -1,62 +1,16 @@
 (function(){
 	angular.module('customersApp')
-	.controller('ordersController', ['$scope', '$routeParams', function($scope, $routeParams){
+	.controller('ordersController', ['$scope', '$routeParams', 'customersFactory', function($scope, $routeParams, customersFactory){
 		
-		var customerId = $routeParams.customerId
-		$scope.orders = null
+	var customerId = $routeParams.customerId
+	$scope.customer = null;
 
-		function init() {
-			//search for customers for the customerId
-			for (var i=0; len=$scope.customers.length;i++){
-				if($scope.customers[i].id===parseInt(customerId)){
-					$scope.orders = $scope.customers[i].orders
-					break
-				}
-			}
-		}
+	function init() {
+		//search for customers for the customerId
+		$scope.customer = customersFactory.getCustomer(customerId)
+	}
 
-		$scope.customers = [{id: 1,
-							joined: '2008-03-10', 
-							name: 'Steve Freeman', 
-							city: 'Cranford', 
-							orderTotal: 666.6666,
-							orders: [
-								{
-									id:1,
-									product: 'Cannabis',
-									total: 666.6666 
-								}
-							]
-							},
+	init()
 
-							{id:2,
-							joined: '2000-12-02', 
-							name: 'Mick Jagger', 
-							city: 'NYC', 
-							orderTotal: 56.32,
-							orders: [
-								{
-									id:2,
-									product: 'Bong',
-									total: 50.00 
-								},
-								{
-									id:3,
-									product: 'Edibles',
-									total: 6.32 
-								}
-
-							]	
-							},
-
-							{id:3,
-							joined: '2000-12-02',
-							name: 'Pat Doucher', 
-							city: 'Westfield', 
-							orderTotal: 420}]
-
-			init()
-
-		
 	}])
 }())
